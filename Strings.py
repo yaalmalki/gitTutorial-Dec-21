68caa069e6161e0bebe5b41589a05e67b0949119
Yahia <yaam02@gmail.com>
2021-12-27 20:26:34 +0300

    added Strig.py

---
+++ Strings.py
@@ -0,0 +1,26 @@
+#created on 27 Dec 21
+#strings can be represented using single quote, double quote and tripple quote, single quote and double quote are the same
+
+#single quote:
+print('Hello, World!')
+#double quote:
+print("Hello, World!")
+#triple quote for multi-lines strings
+print('''Welcome to
+python programming language''')
+
+#Escape Sequences:
+#we use "\" back slash to escape sequences
+#\' single quote
+#\\ back slash
+#\" double quote
+#\n new line
+#\t tap space
+#\o octet vlaue need to practice 
+#\x hex vlaue need to practice
+#Example :
+print("What\'s your name: ")
+print("type \\ sigle quote")
+print("type \" double quote")
+print("hello \n new line ")
+print("hello \t tap space")
