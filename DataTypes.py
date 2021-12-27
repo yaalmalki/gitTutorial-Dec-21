8fcf39025490db4ed97c84c53062c31fa81e6476
Yahia <yaam02@gmail.com>
2021-12-27 22:36:22 +0300

    DataTypes.py added

---
+++ DataTypes.py
@@ -0,0 +1,27 @@
+#created 27 Dec 21
+# DataTypes, Variables and Identifiers
+#======================================
+#there is no variable decleration in Python
+#for example (a=5), a is variable to store numeric data and assinged 5
+
+a=5
+b=6
+c=a*b
+print(c)
+
+# also we can do multiple assignment if all variables have the same value i.e
+#sum=0
+#flag=0
+#num=0
+# sume=flag=num=0
+
+#however if variables have different values and we want to do it in the same line
+#sum=0
+#flag=1
+#name="Python"
+#version=3.7
+#Example
+#sum,flag,name,version=0,1,"Python",3.7
+
+sum,flag,name,version=0,1,"Python",3.7
+print(name, flag, version, sum)
