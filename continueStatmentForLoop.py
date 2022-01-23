a5dc221886dd58abe76efb0fb323e2e7d9946fa2
yaalmalki <yaam02@gmail.com>
2022-01-24 00:14:50 +0300

    break and continue .py added

---
+++ continueStatmentForLoop.py
@@ -0,0 +1,8 @@
+#break and continue
+#iterative Statement
+
+for i in range(1,10):
+    if(i==5):
+        continue #when the condition meets (5==5)jump to excute for iteration again 
+    print(i)
+print('Loop Terminates')
