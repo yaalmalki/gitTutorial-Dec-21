f4495c082491ff03acd0552c2bc2dea64d57ebbe
yaalmalki <yaam02@gmail.com>
2022-01-25 00:31:30 +0300

    creating a list using append function

---
+++ creatListUsingAppendFun.py
@@ -0,0 +1,7 @@
+# create a list of numaric elements using append() and input() to read the element at runtime
+
+seq=[]
+for i in range (5):
+    ele=input('enter 5 elements to be added to the list\n')
+    seq.append(ele)
+print("you have entered thes elements to the list: ", seq)
